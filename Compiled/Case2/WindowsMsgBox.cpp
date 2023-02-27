518cf2e70fc010d436f0dcd26825db6bb0dffe83
Ali Hadi <ali@ashemery.com>
2023-02-26 22:57:21 -0500

    Compiled Code

---
+++ Compiled/Case2/WindowsMsgBox.cpp
@@ -0,0 +1,12 @@
+﻿#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow)
+{
+    FreeConsole();
+    while (TRUE)
+    {
+        MessageBoxW(0, L"CLOSE ME?", L"¯\\___(ツ)___/¯", 0);
+    }
+    
+    return 0;
+}
